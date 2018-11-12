e263b5639b30dabdbb31dd235b477549e5a23b7f
nnshah7 <nnshah7@illinois.edu>
2018-11-11 18:51:24 -0600

    finish ObstetricOfficeVisitValidator

--- iTrust/src/main/edu/ncsu/csc/itrust/model/old/validate/ObstetricOfficeVisitValidator.java
+++ iTrust/src/main/edu/ncsu/csc/itrust/model/old/validate/ObstetricOfficeVisitValidator.java
@@ -15,8 +15,16 @@ public class ObstetricOfficeVisitValidator extends BeanValidator<ObstetricOffice
 	public void validate(ObstetricOfficeVisitBean oov) throws FormValidationException {
 		ErrorList errorList = new ErrorList();
 		
+		errorList.addIfNotNull(checkFormat("obstetricRecordID", oov.getObstetricRecordID(), ValidationFormat.POSITIVE_NONZERO_INT, false));
+		errorList.addIfNotNull(checkFormat("visit id", oov.getVisitId(), ValidationFormat.POSITIVE_NONZERO_INT, false));
 		
-		
+		if(oov.getNumberOfBabies() <= 0) {
+			errorList.addIfNotNull("Number of babies must be greater than 0!");
+		}
+		if(oov.getWeight() <= 0) {
+			errorList.addIfNotNull("Weight must be greater than 0!");
+		}
+
 		if (errorList.hasErrors())
 			throw new FormValidationException(errorList);
 	}
