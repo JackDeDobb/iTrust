ddaa198cf8fb83a66260ee737a229e79eb1cf000
nnshah7 <nnshah7@illinois.edu>
2018-11-11 22:03:26 -0600

    mostly finish ViewObstetricOfficeVisitAction

--- iTrust/src/main/edu/ncsu/csc/itrust/action/ViewObstetricOfficeVisitAction.java
+++ iTrust/src/main/edu/ncsu/csc/itrust/action/ViewObstetricOfficeVisitAction.java
@@ -5,6 +5,7 @@ import java.util.List;
 import edu.ncsu.csc.itrust.exception.ITrustException;
 import edu.ncsu.csc.itrust.model.old.dao.DAOFactory;
 import edu.ncsu.csc.itrust.model.old.dao.mysql.ObstetricOfficeVisitDAO;
+import edu.ncsu.csc.itrust.model.old.dao.mysql.UltrasoundRecordDAO;
 import edu.ncsu.csc.itrust.model.old.beans.ObstetricOfficeVisitBean;
 import edu.ncsu.csc.itrust.model.old.beans.UltrasoundRecordBean;
 import edu.ncsu.csc.itrust.exception.DBException;
@@ -12,12 +13,16 @@ import edu.ncsu.csc.itrust.exception.DBException;
 public class ViewObstetricOfficeVisitAction {
 	
     private ObstetricOfficeVisitDAO obstetricOfficeVisitDAO;
+    private UltrasoundRecordDAO ultrasoundRecordDAO;
     private long loggedInMID;
     private long patientMID;
+    
+    private long currentVisitId;
     
 	public ViewObstetricOfficeVisitAction(DAOFactory factory, long loggedInMID, long patientMID)
 			throws ITrustException {
 		this.obstetricOfficeVisitDAO = factory.getObstetricsOfficeVisitDAO();
+		this.ultrasoundRecordDAO = factory.getUltrasoundRecordDAO();
 		this.loggedInMID = loggedInMID;
 		this.patientMID = patientMID;
 	}
@@ -46,12 +51,19 @@ public class ViewObstetricOfficeVisitAction {
 		int idx = Integer.parseInt(index);
 		List<ObstetricOfficeVisitBean> visitList = getObstetricOfficeVisitRecords();
 		if(visitList.size() > idx) {
-			return visitList.get(idx);
+			ObstetricOfficeVisitBean visit = visitList.get(idx);
+			currentVisitId = visit.getVisitId();
 		}
 		return null;
 	}
 
 	public List<UltrasoundRecordBean> getUltrasoundRecords() {
-		return null;
+		try {
+			List<UltrasoundRecordBean> recordList = 
+					ultrasoundRecordDAO.getUltrasoundRecordsByVisitID(currentVisitId);
+			return recordList;
+		} catch (DBException e) {
+			return null;
+		}
 	}
 }
