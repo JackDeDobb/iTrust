2823d73f0a198dca0f1264fa3b60b959b066c417
nnshah7 <nnshah7@illinois.edu>
2018-11-27 16:49:13 -0600

    ChildBirthVisitValidator

--- iTrust/src/main/edu/ncsu/csc/itrust/model/old/validate/ChildBirthVisitValidator.java
+++ iTrust/src/main/edu/ncsu/csc/itrust/model/old/validate/ChildBirthVisitValidator.java
@@ -3,6 +3,7 @@ package edu.ncsu.csc.itrust.model.old.validate;
 import edu.ncsu.csc.itrust.exception.ErrorList;
 import edu.ncsu.csc.itrust.exception.FormValidationException;
 import edu.ncsu.csc.itrust.model.old.beans.ChildBirthVisitBean;
+import edu.ncsu.csc.itrust.model.old.enums.DeliveryType;
 
 public class ChildBirthVisitValidator extends BeanValidator<ChildBirthVisitBean> {
 	/**
@@ -15,16 +16,10 @@ public class ChildBirthVisitValidator extends BeanValidator<ChildBirthVisitBean>
 	public void validate(ChildBirthVisitBean c) throws FormValidationException {
 		ErrorList errorList = new ErrorList();
 		
-		errorList.addIfNotNull(checkFormat("obstetricRecordID", oov.getObstetricRecordID(), ValidationFormat.POSITIVE_NONZERO_INT, false));
-		errorList.addIfNotNull(checkFormat("visit id", oov.getVisitId(), ValidationFormat.POSITIVE_NONZERO_INT, false));
+		if(c.getPreferredDeliveryType() == DeliveryType.NS) {
+			errorList.addIfNotNull("Preferred delivery type must be set!");
+		}
 		
-		if(oov.getNumberOfBabies() <= 0) {
-			errorList.addIfNotNull("Number of babies must be greater than 0!");
-		}
-		if(oov.getWeight() <= 0) {
-			errorList.addIfNotNull("Weight must be greater than 0!");
-		}
-
 		if (errorList.hasErrors())
 			throw new FormValidationException(errorList);
 	}
