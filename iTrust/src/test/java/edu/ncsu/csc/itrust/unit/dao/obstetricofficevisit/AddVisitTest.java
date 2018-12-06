4fdeff0e05c43e66b4e6f9e249897ba38b9069cd
Yucheng Wang <yw3@illinois.edu>
2018-12-05 21:25:45 -0600

    Add unit test for ObstetricOfficeVisitDao

---
+++ iTrust/src/test/java/edu/ncsu/csc/itrust/unit/dao/obstetricofficevisit/AddVisitTest.java
@@ -0,0 +1,31 @@
+package edu.ncsu.csc.itrust.unit.dao.obstetricofficevisit;
+
+import edu.ncsu.csc.itrust.model.old.beans.ObstetricOfficeVisitBean;
+import edu.ncsu.csc.itrust.model.old.dao.mysql.ObstetricOfficeVisitDAO;
+import edu.ncsu.csc.itrust.unit.testutils.TestDAOFactory;
+import junit.framework.TestCase;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+public class AddVisitTest extends TestCase {
+    ObstetricOfficeVisitDAO obstetricOfficeVisitDAO = TestDAOFactory.getTestInstance().getObstetricsOfficeVisitDAO();
+
+    public void testAdd() throws Exception{
+        ObstetricOfficeVisitBean bean = new ObstetricOfficeVisitBean();
+        bean.setPatientMID(11111111);
+        bean.setHcpMID(12222);
+        bean.setObstetricRecordID(222222);
+        bean.setWeight((float) 100.0);
+        bean.setFetalHeartRate(90);
+        bean.setLowLyingPlacentaObserved(33);
+        bean.setNumberOfBabies(8);
+        Timestamp curr = new Timestamp(System.currentTimeMillis());
+        bean.setVisitDate(curr);
+        obstetricOfficeVisitDAO.addObstetricOfficeVisit(bean);
+
+        List<ObstetricOfficeVisitBean> list = obstetricOfficeVisitDAO.getObstetricOfficeVisitsByPatientMID(11111111);
+        bean = list.get(list.size() - 1);
+        assertEquals((float)100.0, bean.getWeight());
+    }
+}
