9e961abebc7ee4da7f03d43fd0cc80b9bac71b00
Arvind Vijayakumar <arvindvjkumar@gmail.com>
2018-12-09 19:39:19 -0600

    Updated the ultrasound servlet and added a view ultrasound component

--- iTrust/src/main/edu/ncsu/csc/itrust/server/DisplayUltrasoundServlet.java
+++ iTrust/src/main/edu/ncsu/csc/itrust/server/DisplayUltrasoundServlet.java
@@ -1,4 +1,17 @@
 package edu.ncsu.csc.itrust.server;
 
-public class DisplayUltrasoundServlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class DisplayUltrasoundServlet extends HttpServlet {
+
+    private String baseFilePath = ImageStore.baseFilePath;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
 }
--- iTrust/src/main/edu/ncsu/csc/itrust/server/UltrasoundServlet.java
+++ iTrust/src/main/edu/ncsu/csc/itrust/server/UltrasoundServlet.java
@@ -1,29 +1,28 @@
 package edu.ncsu.csc.itrust.server;
 
 import edu.ncsu.csc.itrust.action.AddObstetricOfficeVisitAction;
+import edu.ncsu.csc.itrust.exception.DBException;
 import edu.ncsu.csc.itrust.exception.FormValidationException;
 import edu.ncsu.csc.itrust.model.old.beans.UltrasoundRecordBean;
 import edu.ncsu.csc.itrust.model.old.dao.DAOFactory;
 import edu.ncsu.csc.itrust.model.old.validate.UltrasoundRecordValidator;
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.FileUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
 
-@WebServlet("/upload")
 @MultipartConfig
 public class UltrasoundServlet extends HttpServlet {
 
@@ -33,13 +32,14 @@ public class UltrasoundServlet extends HttpServlet {
     // File upload constants.
     private int maxFileSize = 100000 * 1024;
     private int maxMemSize = 100000 * 1024;
-    private String filePath;
+    private String baseFilePath;
     private UltrasoundRecordValidator validator = new UltrasoundRecordValidator();
 
     @Override
     public void init() throws ServletException {
         super.init();
-        filePath = "/Users/arvind/Documents/College_2015/current_classes/cs427/itrust/T808/temp";
+        // TODO: Replace hardcoded path with system-independent HOME/temp based path.
+        baseFilePath = ImageStore.baseFilePath;
         validator = new UltrasoundRecordValidator();
     }
 
@@ -60,103 +60,100 @@ public class UltrasoundServlet extends HttpServlet {
 
     /**
      * Create an ultrasound record bean populated with the data from the request.
+     * Note: Image path is not populated from text form fields, but created after parsing file part of request.
      */
-    private UltrasoundRecordBean createUltrasoundRecord(HttpServletRequest req) {
+    private UltrasoundRecordBean createUltrasoundRecord(HttpServletRequest request) {
         // Create ultrasound record bean from request params.
-		UltrasoundRecordBean ultrasoundRecord = new UltrasoundRecordBean();
-		Map<Object, Object> params = req.getParameterMap();
-		for (Map.Entry<Object, Object> entry : params.entrySet()) {
-            System.out.println("Key: " + entry.getKey().toString());
-            System.out.println("Value: " + entry.getValue().toString());
-        }
+        UltrasoundRecordBean ultrasoundRecord = new UltrasoundRecordBean();
 
-        ultrasoundRecord.setAbdominalCircumference(Float.valueOf(req.getParameter("abdominalCircumference")));
-        ultrasoundRecord.setBiparietalDiameter(Float.valueOf(req.getParameter("biparietalDiameter")));
-        ultrasoundRecord.setCrownRumpLength(Float.valueOf(req.getParameter("crownRumpLength")));
-        ultrasoundRecord.setEstimatedFetalWeight(Float.valueOf(req.getParameter("estimatedFetalWeight")));
-        ultrasoundRecord.setFemurLength(Float.valueOf(req.getParameter("femurLength")));
-        ultrasoundRecord.setHeadCircumference(Float.valueOf(req.getParameter("headCircumference")));
-        ultrasoundRecord.setHumerusLength(Float.valueOf(req.getParameter("humerusLength")));
-        ultrasoundRecord.setOccipitofrontalDiameter(Float.valueOf(req.getParameter("occipitofrontalDiameter")));
+        ultrasoundRecord.setAbdominalCircumference(Float.valueOf(request.getParameter("abdominalCircumference")));
+        ultrasoundRecord.setBiparietalDiameter(Float.valueOf(request.getParameter("biparietalDiameter")));
+        ultrasoundRecord.setCrownRumpLength(Float.valueOf(request.getParameter("crownRumpLength")));
+        ultrasoundRecord.setEstimatedFetalWeight(Float.valueOf(request.getParameter("estimatedFetalWeight")));
+        ultrasoundRecord.setFemurLength(Float.valueOf(request.getParameter("femurLength")));
+        ultrasoundRecord.setHeadCircumference(Float.valueOf(request.getParameter("headCircumference")));
+        ultrasoundRecord.setHumerusLength(Float.valueOf(request.getParameter("humerusLength")));
+        ultrasoundRecord.setOccipitofrontalDiameter(Float.valueOf(request.getParameter("occipitofrontalDiameter")));
+        ultrasoundRecord.setVisitID(Long.parseLong(request.getParameter("visitId")));
 
         return ultrasoundRecord;
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-//        UltrasoundRecordBean ultrasoundRecord = createUltrasoundRecord(req);
+        // Ultrasound record entry filled with form details.
+        UltrasoundRecordBean ultrasoundRecord = createUltrasoundRecord(request);
 
-//        try {
-//            validator.validate(ultrasoundRecord);
-//        } catch (FormValidationException ex) {
-//            // TODO: Send response signalling validation error.
-//        }
+        // Get logged in mid, and update the action.
+        Long loggedInMID = Long.parseLong(request.getParameter("loggedInMID"));
+        addObstetricOfficeVisitAction.setLoggedInMID(loggedInMID);
 
-        boolean isMultipart = ServletFileUpload.isMultipartContent(req);
-        resp.setContentType("text/plain");
-        PrintWriter out = resp.getWriter();
+        // Get patient mid.
+        Long patientMID = Long.parseLong(request.getParameter("patientMID"));
 
-        if (!isMultipart) {
+        // Response writer.
+        PrintWriter out = response.getWriter();
+
+        try {
+            validator.validate(ultrasoundRecord);
+        } catch (FormValidationException ex) {
+            out.println("Form validation exception: " + ex.getMessage());
             return;
         }
 
-        // Disk items.
-        DiskFileItemFactory factory = new DiskFileItemFactory();
+        // Check if form is multipart (ie. contains a file).
+        boolean isMultipart = ServletFileUpload.isMultipartContent(request);
 
-        // Set max mem size threshold.
-        factory.setSizeThreshold(maxMemSize);
+        if (!isMultipart) {
+            out.println("Incorrect request type: Request type must be multipart.");
+            return;
+        }
 
-        // Location to save if mem size is not sufficient.
+        // Parse file part.
+        Part filePart = request.getPart("file");
 
-        File tempFile = File.createTempFile(filePath, File.pathSeparator + "temp");
-        tempFile.mkdirs();
-        factory.setRepository(tempFile);
+        // Check if file is provided.
+        if (filePart == null) {
+            // If no file is provided.
+            // Set image path to empty, to indicate no files there.
+            ultrasoundRecord.setImagePath("");
 
-        // Create a new file upload handler.
-        ServletFileUpload upload = new ServletFileUpload(factory);
+        } else {
+            // If file is provided.
+            String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString();
 
-        // Maximum file size to be uploaded.
-        upload.setSizeMax( maxFileSize );
+            // Create relative image path.
+            String relImagePath = Paths.get(ultrasoundRecord.getVisitID() + "", fileName).toString();
 
-//        // Visit id to be used with ultrasound id.
-//        long visitId = Long.parseLong(req.getParameter("visitId"));
-//
-//        // Patient id to be used with ultrasound.
-//        long patientMID = Long.parseLong(req.getParameter("patientMID"));
+            // Create absolute path.
+            Path absolutePath = Paths.get(baseFilePath, relImagePath);
+            System.out.println(absolutePath.toString());
 
-        try {
-            // Parse the request to get file items.
-            List<FileItem> fileItems = upload.parseRequest(req);
+            // Create visitId dir.
+            File visitIdDir = absolutePath.toFile().getParentFile();
+            visitIdDir.mkdirs();
 
-            // Process the uploaded file items
-            Iterator<FileItem> i = fileItems.iterator();
+            // Clear visitId directory.
+            FileUtils.cleanDirectory(visitIdDir);
 
-            File file = null;
-            while ( i.hasNext () ) {
-                FileItem fi = i.next();
-                if ( !fi.isFormField () ) {
-                    // Get the uploaded file parameters
-                    String fieldName = fi.getFieldName();
-                    String fileName = fi.getName();
-                    String contentType = fi.getContentType();
-                    boolean isInMemory = fi.isInMemory();
-                    long sizeInBytes = fi.getSize();
-
-                    // Write the file
-                    String uniqueFilename = FilepathGen.generateUniqueFileName(fileName);
-                    file = Paths.get(filePath,uniqueFilename).toFile();
-                    System.out.println("Absolute Path: " + file.getAbsolutePath());
-                    file.getParentFile().mkdirs();
-                    fi.write(file);
-                    out.println(file.getAbsolutePath());
-                }
+            // Write file.
+            try (InputStream fileContent = filePart.getInputStream()) {
+                Files.copy(fileContent, absolutePath);
             }
 
-//            ultrasoundRecord.setImagePath(file.getAbsolutePath());
-//            addObstetricOfficeVisitAction.addUltrasoundRecord(visitId, patientMID, ultrasoundRecord);
-        } catch(Exception ex) {
-            System.out.println(ex);
+            // Set ultrasound record field.
+            ultrasoundRecord.setImagePath(relImagePath);
         }
+
+
+        try {
+            addObstetricOfficeVisitAction.addUltrasoundRecord(patientMID, ultrasoundRecord);
+            out.println("Successfully added ultrasound record.");
+        } catch (FormValidationException | DBException ex) {
+            out.println("Encountered exception on adding record: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+
     }
 }
